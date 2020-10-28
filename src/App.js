6951b1c3fa88b733a128850bda0de3ca2f891d5f
Ton Nattaphon <ton.edu63@gmail.com>
2020-10-28 18:09:30 +0700

    Loginpage

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import Loginform from './components/Loginform';
+
+function App() {
+  const admin = {
+    email: "admin@admin.com",
+    password:"admin123"
+  }
+
+  const [user, setUser] = useState({name:"", email:""});
+  const [error, setError] = useState("");
+  
+  const Login = details =>{
+    console.log(details);
+  }
+  const Logout = () =>{
+    console.log("Logout");
+ }
+  
+  return (
+    <div className="App">
+       {(user.email != "") ? (
+         <div className="welcome">
+           <h2>Welcome, <span>{user.name}</span></h2>
+           <button>Logout</button>
+         </div>
+       ) : <Loginform Login={Login} error={error}/>}
+    </div>
+  );
+}
+
+export default App;
